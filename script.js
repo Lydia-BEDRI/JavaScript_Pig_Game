f18441f2b3a4dcf0839394b970abe11d8a76d423
Lady <bedrilydia118@gmail.com>
2024-06-09 00:42:23 +0200

    Switching the active player

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
-
+let activePlayer = 0;
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
     //1. Generating a random dice roll
@@ -29,10 +31,18 @@ btnRoll.addEventListener('click', function () {
     // Add dice to the current score
     if (dice !== 1) {
         currentScore += dice;
-        current0El.textContent = currentScore;
+
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
     }
     //1: if true , switch to the next player
     else {
+
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
 
     }
 
