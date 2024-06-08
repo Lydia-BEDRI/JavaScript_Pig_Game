7b88a590a1f50920b1dad09fb8a788bd27bdecc8
Lady <bedrilydia118@gmail.com>
2024-06-09 00:11:12 +0200

    Rolling dice

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    //1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    // 2. Dispaly dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `img/dice-${dice}.png`;
+    // 3. Check for rolled  
+    // Add dice to the current score
+    if (dice !== 1) {
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    }
+    //1: if true , switch to the next player
+    else {
+
+    }
+
+
+});
+
